0b1cc497ba55ef0353f24c3ab310900e58a99c5f
jean-eudes <jeudes7@gmail.com>
2024-12-02 22:15:59 +0100

    create first route

--- s3/src/main.rs
+++ s3/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    routing::{get, put},
+    Router,
+};
+
+async fn get_object<'a>() -> &'a str {
+    "Hello, World!"
+}
+
+async fn put_object<'a>() -> &'a str {
+    "Hello, World put"
+}
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(get_object))
+        .route("/", put(put_object));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
