27b9300f50acd9b441010c690b82216bb91ede0e
jean-eudes <jeudes7@gmail.com>
2024-12-08 15:14:00 +0100

    first iteration of bucket.

--- s3/src/main.rs
+++ s3/src/main.rs
@@ -1,3 +1,5 @@
+use axum::extract::State;
+use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{
     body::Body,
@@ -6,21 +8,31 @@ use axum::{
     Router,
 };
 use byteorder::{BigEndian, ReadBytesExt};
-use bytes::{Buf, BufMut, BytesMut};
-use foundationdb::RangeOption;
+use bytes::{BufMut, BytesMut};
+use foundationdb::directory::{Directory, DirectoryLayer};
+use foundationdb::tuple::Subspace;
+use foundationdb::{Database, RangeOption};
 use futures::stream::StreamExt;
 use std::io::Cursor;
+use std::sync::Arc;
 use std::usize;
-use foundationdb::tuple::{pack, Subspace};
 
 const MAX_SIZE: usize = 90 * 1024;
 
-async fn get_object<'a>() -> &'a str {
-    "Hello, World! "
+#[derive(Clone)]
+struct AppState {
+    database: Arc<Database>,
 }
 
-async fn download(Path(file_name): Path<String>) -> impl IntoResponse {
-    let db = foundationdb::Database::default().unwrap();
+async fn get_object<'a>() -> &'a str {
+    "Hello, World!"
+}
+
+async fn get_bucket(
+    State(state): State<AppState>,
+    Path(file_name): Path<String>,
+) -> impl IntoResponse {
+    let db = state.database;
 
     let trx = db.create_trx().unwrap();
     let file_name_key = Subspace::from((&file_name, "data"));
@@ -29,25 +41,69 @@ async fn download(Path(file_name): Path<String>) -> impl IntoResponse {
 
     let mut x = trx.get_ranges_keyvalues(opt, false);
 
-    let size = trx
-        .get(format!("{}/size", &file_name).as_bytes(), false)
-        .await;
-    let vec = size.unwrap().unwrap().to_vec();
-    let i = Cursor::new(vec).read_uint::<BigEndian>(8).unwrap();
-    println!("download file {} with size {}", &file_name, i);
     let mut vec = vec![];
     while let Some(message) = x.next().await {
         let value = message.unwrap();
         let data = value.value();
-        vec.extend(data)
+        vec.put(data)
     }
 
     let body = Body::from(vec);
     body
 }
 
-async fn put_object(Path(file_name): Path<String>, body: Body) -> String {
-    let db = foundationdb::Database::default().unwrap();
+async fn create_bucket(
+    State(state): State<AppState>,
+    Path(bucket): Path<String>,
+) -> impl IntoResponse {
+    let db = state.database;
+
+    let directory = DirectoryLayer::default();
+    let trx = db.create_trx().unwrap();
+
+    let new_directory = directory.create(&trx, &[bucket], None, None).await;
+    match new_directory {
+        Ok(dir) => {
+            (StatusCode::CREATED, dir.get_path().join("/").to_string())
+        }
+        Err(_e) => {
+            (StatusCode::INTERNAL_SERVER_ERROR, "r".to_string())
+        }
+    }
+
+}
+
+async fn download(
+    State(state): State<AppState>,
+    Path((bucket, file_name)): Path<(String, String)>,
+) -> impl IntoResponse {
+    let db = state.database;
+
+    let trx = db.create_trx().unwrap();
+    let file_name_key = Subspace::from((&bucket, &file_name, "data"));
+
+    let opt = RangeOption::from(file_name_key.range());
+
+    let mut x = trx.get_ranges_keyvalues(opt, false);
+
+    let mut vec = vec![];
+    while let Some(message) = x.next().await {
+        let value = message.unwrap();
+        let data = value.value();
+        vec.put(data)
+    }
+
+    let body = Body::from(vec);
+    body
+}
+
+async fn put_object(
+    State(state): State<AppState>,
+    Path((bucket, file_name)): Path<(String, String)>,
+    body: Body,
+) -> (StatusCode, String) {
+    let db = state.database;
+    println!("download file {}", &file_name);
 
     let mut stream = body.into_data_stream();
     let mut buffer = BytesMut::with_capacity(MAX_SIZE);
@@ -55,10 +111,9 @@ async fn put_object(Path(file_name): Path<String>, body: Body) -> String {
     let transaction = db.create_trx().unwrap();
     let mut part = 1;
     let mut size: usize = 0;
-    let file_name_key = Subspace::from(&file_name);
+    let file_name_key = Subspace::from((&bucket, &file_name));
     let data_key = file_name_key.subspace(&"data");
     while let Some(message) = stream.next().await {
-        println!("download file {}", &file_name);
         let mut data = &message.unwrap()[..];
         size += data.len();
         if buffer.len() + data.len() < MAX_SIZE {
@@ -68,7 +123,6 @@ async fn put_object(Path(file_name): Path<String>, body: Body) -> String {
 
         if buffer.len() + data.len() == MAX_SIZE {
             buffer.put_slice(&data);
-            // let key = (&data_key, part);
             transaction.set(&data_key.pack(&part), &buffer[..]);
             part = part + 1;
             buffer.clear();
@@ -86,7 +140,6 @@ async fn put_object(Path(file_name): Path<String>, body: Body) -> String {
 
         let remaining = &data[0..data.len()];
         buffer.put_slice(remaining);
-
     }
     transaction.set(
         format!("{}/size", &file_name).as_bytes(),
@@ -103,25 +156,34 @@ async fn put_object(Path(file_name): Path<String>, body: Body) -> String {
 
     match commit {
         Ok(_) => {
-            println!("commit success")
+            println!("commit success");
+            (StatusCode::CREATED, file_name)
         }
         Err(e) => {
-            eprintln!("commit failed, {}", e)
+            eprintln!("commit failed, {}", e);
+            (StatusCode::INTERNAL_SERVER_ERROR, file_name)
         }
     }
-
-    file_name
 }
 
 #[tokio::main]
 async fn main() {
     let network = unsafe { foundationdb::boot() };
 
+    let db = Database::default().unwrap();
+
+    let state = AppState {
+        database: Arc::new(db),
+    };
+
     // build our application with a single route
     let router = Router::new()
         .route("/", get(get_object))
-        .route("/:file_name", put(put_object))
-        .route("/:file_name", get(download));
+        .route("/:bucket/:file_name", put(put_object))
+        .route("/:bucket/:file_name", get(download))
+        .route("/:bucket", put(create_bucket))
+        .route("/:bucket", get(get_bucket))
+        .with_state(state);
 
     println!("start service on port 3000");
     // run our app with hyper, listening globally on port 3000
